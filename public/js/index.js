c4e49a98cb36218c04c2b0f2e62962683897481b
Dillon Hess <dhess268@gmail.com>
2022-06-12 23:02:27 -0400

    finally add server and DB functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,28 @@
 var socket = io();
+
+
+const button = document.querySelector('#fGersButton')
+const h2 = document.querySelector('#counter')
+button.addEventListener('click', dealWithFGers)
+
+
+function dealWithFGers() {
+    socket.emit('fGerson')
+
+    let count = Number(h2.innerText)
+    count += 1
+    h2.innerText = count
+}
+
+
+socket.on('connected', (count) => {
+    console.log(count)
+    h2.innerText = count
+})
+
+
+socket.on('fGers', () => {
+    let count = Number(h2.innerText)
+    count += 1
+    h2.innerText = count
+})
--- server.js
+++ server.js
@@ -16,32 +16,94 @@ const io = new Server(server);
 const PORT = process.env.PORT || 8000;
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-
-const path = require('path')
+require('dotenv').config()
+const path = require('path');
+const { resolve } = require('path');
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use(express.static(path.join(__dirname, 'public')))
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-//   });
-MongoClient.connect('mongodb+srv://dhess:Buanimation17!@cluster0.4zb2b.mongodb.net/?retryWrites=true&w=majority',{useUnifiedTopology: true})
+const setCount = 0
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'gersonDB'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
 .then(client => {
-    const db = client.db('gerson-db')
-    io.on('connection', (socket) => {
+    console.log(`Connected to ${dbName} Database`)
+    db = client.db(dbName)
+    const getsonCounters = db.collection('counts')
+
     
-        io.on('fGerson', () => {
-            socket.broadcast.emit('fGers')
-        })
+io.on('connection', (socket) => {
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
     
     
-        console.log('Connected to server')
+
+    db.collection('counts').find().toArray()
+    .then(result => {
+        // console.log(result[0].counter)
+        count = result[0].counter
+        socket.emit('connected', count) 
     })
 
+    
+    socket.on('fGerson', () => {
+            db.collection('counts').findOneAndUpdate(
+                {},
+                {
+                  $inc: {
+                    counter: 1
+                  }
+                },
+                {
+                  upsert: true
+                }
+              )
+                .then(result => {
+                    socket.broadcast.emit('fGers')})
+                .catch(error => console.error(error))   
+    })
+
+    // app.post('/fgers', (req, res) => {
+    //     console.log(req.body)
+    //     db.collection('counts').insertOne(req.body)
+    //       .then(result => {
+    //         res.json('Success')
+    //       })
+    //       .catch(error => console.error(error))
+    //   })
+
+
+
+
+
+    console.log('Connected to server')
+})
 })
 
 
+
+function getCount() {
+    let count = 0
+    console.log(db.collection('counts').find().toArray()
+        .then(result => {
+            // console.log(result[0].counter)
+            count = result[0].counter
+            return count
+        }))
+
+        
+        
+}
 
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
+
+function getCount(){
+    
+}
